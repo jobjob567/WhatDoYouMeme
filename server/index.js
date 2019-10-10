eb489b2b55b9069349b5027eab2547d17a79b40f
Job Giuliani <giulianj4@hawkmail.newpaltz.edu>
2019-10-10 17:05:02 -0400

    problem with fixing? according to compile

--- server/index.js
+++ server/index.js
@@ -4,9 +4,12 @@ const userController = require('./controllers/Users');
 const gameController = require('./controllers/Game');
 
 const app = expres();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app 
+    .get('/port', (req,res) => {
+        return res.send("Using port: " + port);
+    })
     .use('/static', express.static(path.join(__dirname, '../NoFramework')))
     .use('/users' , userController);
     .use('/game', gameController)
